c16ec6ad8b0e3944f4e90a7c3255067041dadaff
tobsirl <tobster111@gmail.com>
2020-03-06 15:13:09 +0000

    House component now displays the data from houses.json

--- components/House.js
+++ components/House.js
@@ -1,6 +1,13 @@
-const House = () => (
+const House = ({ title, picture, type, town, rating, reviewsCount }) => (
   <div>
-    <h2>House</h2>
+    <img src={picture} width="100%" alt="House picture" />
+    <p>
+      {type} - {town}
+    </p>
+    <p>{title}</p>
+    <p>
+      {rating} ({reviewsCount})
+    </p>
   </div>
 );
 
