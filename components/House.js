295b57ea742eb4371a51c9a8651cb1a537033cc3
tobsirl <tobster111@gmail.com>
2020-03-07 15:15:31 +0000

    Added an id to the houses data, using Link in House.js to provide clickable links on the houses

--- components/House.js
+++ components/House.js
@@ -1,14 +1,20 @@
-const House = ({ title, picture, type, town, rating, reviewsCount }) => (
-  <div>
-    <img src={picture} width="100%" alt="House picture" />
-    <p>
-      {type} - {town}
-    </p>
-    <p>{title}</p>
-    <p>
-      {rating} ({reviewsCount})
-    </p>
-  </div>
-);
+import Link from 'next/link';
+
+const House = ({ id, title, picture, type, town, rating, reviewsCount }) => {
+  return (
+    <Link href="/houses/[id]" as={`/houses/${id}`}>
+      <a>
+        <img src={picture} width="100%" alt="House picture" />
+        <p>
+          {type} - {town}
+        </p>
+        <p>{title}</p>
+        <p>
+          {rating} ({reviewsCount})
+        </p>
+      </a>
+    </Link>
+  );
+};
 
 export default House;
