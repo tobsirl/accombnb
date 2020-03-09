d5ab36fc21c71d68ffd2a3a4a70b3a428f8fd0ea
tobsirl <tobster111@gmail.com>
2020-03-09 15:28:07 +0000

    Adding state to the component using useState hook

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
 
@@ -13,43 +14,48 @@ const parseDate = (str, format, locale) => {
 const formatDate = (date, format, locale) =>
   dateFnsFormat(date, format, { locale });
 
-export default () => (
-  <div className="date-range-picker-container">
-    <div>
-      <label>From:</label>
-      <DayPickerInput
-        formatDate={formatDate}
-        format={format}
-        parseDate={parseDate}
-        placeholder={`${dateFnsFormat(new Date(), format)}`}
-      />
+export default () => {
+  const [stateDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  return (
+    <div className="date-range-picker-container">
+      <div>
+        <label>From:</label>
+        <DayPickerInput
+          formatDate={formatDate}
+          format={format}
+          parseDate={parseDate}
+          placeholder={`${dateFnsFormat(new Date(), format)}`}
+        />
+      </div>
+      <div>
+        <label>To:</label>
+        <DayPickerInput
+          formatDate={formatDate}
+          format={format}
+          parseDate={parseDate}
+          placeholder={`${dateFnsFormat(new Date(), format)}`}
+        />
+      </div>
+      <style jsx>{`
+        .date-range-picker-container div {
+          display: grid;
+          border: 1px solid #ddd;
+          grid-template-columns: 30% 70%;
+          padding: 10px;
+        }
+        label {
+          padding-top: 10px;
+        }
+      `}</style>
+      <style jsx global>{`
+        .DayPickerInput input {
+          width: 120px;
+          padding: 10px;
+          font-size: 16px;
+        }
+      `}</style>
     </div>
-    <div>
-      <label>To:</label>
-      <DayPickerInput
-        formatDate={formatDate}
-        format={format}
-        parseDate={parseDate}
-        placeholder={`${dateFnsFormat(new Date(), format)}`}
-      />
-    </div>
-    <style jsx>{`
-      .date-range-picker-container div {
-        display: grid;
-        border: 1px solid #ddd;
-        grid-template-columns: 30% 70%;
-        padding: 10px;
-      }
-      label {
-        padding-top: 10px;
-      }
-    `}</style>
-    <style jsx global>{`
-      .DayPickerInput input {
-        width: 120px;
-        padding: 10px;
-        font-size: 16px;
-      }
-    `}</style>
-  </div>
-);
+  );
+};
