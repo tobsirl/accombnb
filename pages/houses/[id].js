bf1133bf609742e1cd9c472e4402297faa48b86b
tobsirl <tobster111@gmail.com>
2020-03-11 14:50:51 +0000

    Adding state to [id].js using useState

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,47 +1,58 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Layout from '../../components/Layout';
 import DateRangePicker from '../../components/DateRangePicker';
 
 import houses from '../houses.json';
 
-const House = props => (
-  <Layout
-    content={
-      <div className="container">
-        <Head>
-          <title>{props.house.title}</title>
-        </Head>
-        <article>
-          <img src={props.house.picture} width="100%" alt="House picture" />
-          <p>
-            {props.house.type} - {props.house.town}
-          </p>
-          <p>{props.house.title}</p>
-          <p>
-            {props.house.rating} ({props.house.reviewsCount})
-          </p>
-        </article>
+const House = props => {
+  const [dateChosen, setDateChosen] = useState(false);
 
-        <aside>
-          <h2>Add dates for prices</h2>
-          <DateRangePicker />
-        </aside>
-        <style jsx>{`
-          .container {
-            display: grid;
-            grid-template-columns: 60% 40%;
-            grid-gap: 30px;
-          }
+  return (
+    <Layout>
+      content=
+      {
+        <div className="container">
+          <Head>
+            <title>{props.house.title}</title>
+          </Head>
+          <article>
+            <img src={props.house.picture} width="100%" alt="House picture" />
+            <p>
+              {props.house.type} - {props.house.town}
+            </p>
+            <p>{props.house.title}</p>
+            <p>
+              {props.house.rating} ({props.house.reviewsCount})
+            </p>
+          </article>
 
-          aside {
-            border: 1px solid #ccc;
-            padding: 20px;
-          }
-        `}</style>
-      </div>
-    }
-  />
-);
+          <aside>
+            <h2>Add dates for prices</h2>
+            <DateRangePicker
+              datesChanged={(startDate, endDate) => {
+                console.log(startDate, endDate);
+                setDateChosen(true);
+              }}
+            />
+          </aside>
+          <style jsx>{`
+            .container {
+              display: grid;
+              grid-template-columns: 60% 40%;
+              grid-gap: 30px;
+            }
+
+            aside {
+              border: 1px solid #ccc;
+              padding: 20px;
+            }
+          `}</style>
+        </div>
+      }
+    </Layout>
+  );
+};
 
 House.getInitialProps = ({ query }) => {
   const { id } = query;
