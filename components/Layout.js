45723fa7a773d19e2eb391ea1128f8d90430d8d9
tobsirl <tobster111@gmail.com>
2020-03-12 14:45:02 +0000

    Using RegistrationModal and LoginModal in Layout component with conditional rendering, passed as props.children

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 import Modal from './Modal';
 import Header from './Header';
+import LoginModal from './LoginModal';
+import RegistrationModal from './RegistrationModal';
 
 const Layout = props => {
   const [showModal, setShowModal] = useState(true);
   const [showLoginModal, setShowLoginModal] = useState(true);
   const [showRegistrationModal, setShowRegistrationModal] = useState(false);
-  
+
   return (
     <div>
       <Header />
       <main>{props.content}</main>
-      {showModal && <Modal close={() => setShowModal(false)}>test</Modal>}
+      {showModal && (
+        <Modal close={() => setShowModal(false)}>
+          {showLoginModal && <LoginModal />}
+          {showRegistrationModal && <RegistrationModal />}
+        </Modal>
+      )}
 
       <style jsx global>{`
         body {
