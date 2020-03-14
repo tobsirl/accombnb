1a99871f952708bf5e5a58316cc0b528dc9f8b2b
tobsirl <tobster111@gmail.com>
2020-03-14 11:11:00 +0000

    Added login form to the LoginModal.js

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,1 +1,12 @@
-export default () => <p>Login Modal</p>
+export default () => (
+  <>
+    <h2>Log in</h2>
+    <div>
+      <form>
+        <input id="email" type="email" placeholder="Email address" />
+        <input id="password" type="password" placeholder="Password" />
+        <button>Log in</button>
+      </form>
+    </div>
+  </>
+);
