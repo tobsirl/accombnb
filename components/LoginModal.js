2dcc99a05f90e49301cc4eb3e49f75c3bfb2a428
tobsirl <tobster111@gmail.com>
2020-03-15 14:11:29 +0000

    Added onSubmit to LoginModal.js

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -2,7 +2,12 @@ export default props => (
   <>
     <h2>Log in</h2>
     <div>
-      <form>
+      <form
+        onSubmit={event => {
+          alert('Log in!');
+          event.preventDefault();
+        }}
+      >
         <input id="email" type="email" placeholder="Email address" />
         <input id="password" type="password" placeholder="Password" />
         <button>Log in</button>
