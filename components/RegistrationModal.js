bdd907f64ebc02561e1a44957b2b421753e1db48
tobsirl <tobster111@gmail.com>
2020-03-16 17:17:47 +0000

    onChange added to each form(email, password, passwordConfirmation)

--- components/RegistrationModal.js
+++ components/RegistrationModal.js
@@ -15,12 +15,23 @@ export default props => {
             event.preventDefault();
           }}
         >
-          <input id="email" type="email" placeholder="Email address" />
-          <input id="password" type="password" placeholder="Password" />
+          <input
+            id="email"
+            type="email"
+            placeholder="Email address"
+            onChange={event => setEmail(event.target.value)}
+          />
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            onChange={event => setPassword(event.target.value)}
+          />
           <input
             id="passwordconfirmation"
             type="password"
             placeholder="Enter password again"
+            onChange={event => setPasswordConfirmation(event.target.value)}
           />
           <button>Sign up</button>
         </form>
