e4cd1abca7403afb919763b3a6cac4173637cbf4
Paul Tobin <tobster111@gmail.com>
2020-04-21 19:28:59 +0100

    Using express-session to handle the session management

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const next = require('next');
+const session = require('express-session');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -8,6 +9,19 @@ const handle = nextApp.getRequestHandler();
 
 nextApp.prepare().then(() => {
   const server = express();
+
+  server.use(
+    session({
+      secret: 'thecakeisalie',
+      resave: false,
+      saveUninitialized: true,
+      name: 'accombnb',
+      cookie: {
+        secure: false,
+        maxAge: 30 * 24 * 60 * 60 * 1000, 
+      },
+    })
+  );
 
   server.all('*', (req, res) => {
     return handle(req, res);
