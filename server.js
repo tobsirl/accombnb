1215e45f06da7048503d2326370b66b94df918c3
Paul Tobin <tobster111@gmail.com>
2020-04-22 19:47:18 +0100

    Installed passport and passport-local to handle authentication, using passport to find and validate the user

--- server.js
+++ server.js
@@ -4,11 +4,44 @@ const session = require('express-session');
 const redis = require('redis');
 const redisClient = redis.createClient();
 const redisStore = require('connect-redis')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async function (email, password, done) {
+      if (!email || !password) {
+        done('Email and password required', null);
+        return;
+      }
+
+      const user = await User.findOne({ where: { email: email } });
+
+      if (!user) {
+        done('User not found', null);
+        return;
+      }
+
+      const valid = await user.isPasswordValid(password);
+
+      if (!valid) {
+        done('Email and password do not match', null);
+        return;
+      }
+
+      done(null, user);
+    }
+  )
+);
 
 nextApp.prepare().then(() => {
   const server = express();
