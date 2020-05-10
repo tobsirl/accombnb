a14e31e9065a08214d834623fbb9a3fb4bd762a9
Paul Tobin <tobster111@gmail.com>
2020-05-10 15:24:58 +0100

    Added the route for registering a user

--- server.js
+++ server.js
@@ -83,6 +83,30 @@ nextApp.prepare().then(() => {
     passport.session()
   );
 
+  // routes
+  server.post('/api/auth/register', async (req, res) => {
+    const { email, password, passwordconfirmation } = req.body;
+
+    if (password !== passwordconfirmation) {
+      res.end(
+        JSON.stringify({ status: 'error', message: 'Passwords do not match' })
+      );
+      return;
+    }
+
+    try {
+      const user = await User.create({ email, password });
+      res.end(JSON.stringify({ status: 'success', message: 'User added' }));
+    } catch (error) {
+      res.statusCode = 500;
+      let message = 'An error occurred';
+      if (error.name === 'SequelizeUniqueConstraintError') {
+        message = 'User already exists';
+      }
+      res.end(JSON.stringify({ status: 'error', message }));
+    }
+  });
+
   server.all('*', (req, res) => {
     return handle(req, res);
   });
